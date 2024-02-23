81e965030b30fca1f0713035236ff0b9f1170bb0
mudassarali.official <mudassarali.official@gmail.com>
2024-02-23 18:25:18 +0500

    230224-Data Access Layer is Ready

---
+++ ToDo-Auth-DAL/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDo_Auth_DAL.Models;
+
+namespace ToDo_Auth_DAL.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<AuthToDoUserEntity>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<AuthToDoUserEntity> AppUsers { get; set; }
+    }
+}
---
+++ ToDo-Auth-DAL/Repositories/ToDoRepository.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using ToDo_Auth_DAL.DbContext;
+using ToDo_Auth_DAL.IRepositories;
+using ToDo_Auth_DAL.Models;
+
+namespace ToDo_Auth_DAL.Repositories
+{
+    public class ToDoRepository: IToDoRepository<AuthToDoUserEntity>
+    {
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly ILogger _logger;
+
+        public ToDoRepository(ILogger<AuthToDoUserEntity> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<AuthToDoUserEntity> Create(AuthToDoUserEntity appuser)
+        {
+            try
+            {
+                if (appuser != null)
+                {
+                    var obj = _appDbContext.Add<AuthToDoUserEntity>(appuser);
+                    await _appDbContext.SaveChangesAsync();
+                    return obj.Entity;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(AuthToDoUserEntity appuser)
+        {
+            try
+            {
+                if (appuser != null)
+                {
+                    var obj = _appDbContext.Remove(appuser);
+                    if (obj != null)
+                    {
+                        _appDbContext.SaveChangesAsync();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<AuthToDoUserEntity> GetAll()
+        {
+            try
+            {
+                var obj = _appDbContext.AppUsers.ToList();
+                if (obj != null)
+                    return obj;
+                else
+                    return null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public AuthToDoUserEntity GetById(int Id)
+        {
+            try
+            {
+                if (Id != null)
+                {
+                    var Obj = _appDbContext.AppUsers.FirstOrDefault(x => x.Id == Id);
+                    if (Obj != null)
+                        return Obj;
+                    else
+                        return null;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Update(AuthToDoUserEntity appuser)
+        {
+            try
+            {
+                if (appuser != null)
+                {
+                    var obj = _appDbContext.Update(appuser);
+                    if (obj != null)
+                        _appDbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
